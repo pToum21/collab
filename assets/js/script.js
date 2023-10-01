b7b29816cbee9180866b1e009193d116a7aaa909
toum21 <pey2moo1@gmail.com>
2023-10-01 11:27:06 -0700

    added the teams and the odds that the user searches to the application as text content,  also added spreads as the specified market for the odds api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,45 @@
-var seacrhBtn = document.querySelector('#search-btn')
-var searchBar = document.querySelector('#search-bar')
+var seacrhBtn = document.querySelector('#search-btn');
+var searchBar = document.querySelector('#search-bar');
 var team;
-var gameArea = document.querySelector('#game-area')
+var gameArea = document.querySelector('#game-area');
 var americanFootballTeamName;
 var oddsTeam;
-
-
+var teamOne = document.querySelector('#team-1');
+var teamTwo = document.querySelector('#team-2');
+var teamOneOdds = document.querySelector('#odds-for-team-1');
+var teamTwoOdds = document.querySelector('#odds-for-team-2');
 function oddsGetter(teamName) {
   // First API for odds
   var options = { method: "GET", headers: { "User-Agent": "insomnia/8.1.0" } };
 
   fetch(
-    "https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds/?apiKey=2e7e1a581a683ba3a7d10cb86a9f4105&regions=us",
+    "https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds/?apiKey=2e7e1a581a683ba3a7d10cb86a9f4105&regions=us&markets=spreads",
     options
   )
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
+    console.log(data)
       for (let i = 0; i < data.length; i++) {
         if(teamName === data[i].home_team || teamName === data[i].away_team){
+
+
           console.log(data[i].bookmakers[1].markets[0].outcomes[0].name)
         console.log(data[i].bookmakers[1].markets[0].outcomes[1].name)
+        console.log(data[i].bookmakers[1].markets[0].outcomes[0].price)
+        console.log(data[i].bookmakers[1].markets[0].outcomes[1].price)
+
+          teamOne.textContent = data[i].bookmakers[1].markets[0].outcomes[0].name;
+          teamTwo.textContent = data[i].bookmakers[1].markets[0].outcomes[1].name;
+          teamOneOdds.textContent = data[i].bookmakers[1].markets[0].outcomes[0].price;
+          teamTwoOdds.textContent = data[i].bookmakers[1].markets[0].outcomes[1].price;
+
         }else {
           console.log('no odds')
         }
       }
-      console.log(data)
+      
 
   })
   .catch(function (error) {
