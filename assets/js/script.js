ed1bcc32958d5862d69810a894fe83f39bc1b9bf
toum21 <pey2moo1@gmail.com>
2023-09-30 17:14:01 -0700

    updated js to show the api calls and data in the correct format and endpoints inside of the console, as well as updated the html and css layout of the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
 var seacrhBtn = document.querySelector('#search-btn')
 var searchBar = document.querySelector('#search-bar')
+var team;
+var gameArea = document.querySelector('#game-area')
+var americanFootballTeamName;
+var oddsTeam;
 
 
-
-
-function teamchooser() {
+function oddsGetter(teamName) {
   // First API for odds
   var options = { method: "GET", headers: { "User-Agent": "insomnia/8.1.0" } };
 
@@ -12,32 +14,50 @@ function teamchooser() {
     "https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds/?apiKey=2e7e1a581a683ba3a7d10cb86a9f4105&regions=us",
     options
   )
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+      for (let i = 0; i < data.length; i++) {
+        if(teamName === data[i].home_team || teamName === data[i].away_team){
+          console.log(data[i].bookmakers[1].markets[0].outcomes[0].name)
+        console.log(data[i].bookmakers[1].markets[0].outcomes[1].name)
+        }else {
+          console.log('no odds')
+        }
+      }
+      console.log(data)
 
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+}
   //   Second API for games
-  var americanFootBallURL =
-    "https://americanfootballapi.p.rapidapi.com/api/american-football/search/eagles";
-  var americanFootBallKey = {
+  function teamchooser(){
+    var americanFootballURL =
+    "https://americanfootballapi.p.rapidapi.com/api/american-football/search/" + team;
+  var americanFootballKey = {
     method: "GET",
     headers: {
-      "X-RapidAPI-Key": "f1a959becamsh9744d6f06f72784p14c8cajsn1767b0e8978d",
+      "X-RapidAPI-Key": '746925a469msh98c5942603eb21ap1763ecjsn8eaf237b1f64',
       "X-RapidAPI-Host": "americanfootballapi.p.rapidapi.com",
     },
   };
 
-  fetch(americanFootBallURL, americanFootBallKey)
+  fetch(americanFootballURL, americanFootballKey)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
+      americanFootballTeamName = data.results[0].entity.name
+      oddsGetter(americanFootballTeamName);
     })
     .catch(function (error) {
       console.log(error);
-    });
-}
+    });}
+  
+
 
 
 seacrhBtn.addEventListener('click', function (event) {
@@ -45,3 +65,12 @@ seacrhBtn.addEventListener('click', function (event) {
   team = searchBar.value;
   teamchooser();
 })
+
+
+
+
+
+
+// if(americanFootballTeamName === data[0].home_team || data[0].away_team){
+ 
+// }
